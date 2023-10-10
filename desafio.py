b25f74a6b625f838517fb6543f5feda92127be6f
GabrielJulioSantos <145797553+GabrielJulioSantos@users.noreply.github.com>
2023-10-10 11:11:47 -0300

    desafiopy
    ga

---
+++ desafio.py
@@ -0,0 +1,5 @@
+largura=float(input("digite a largura da sua parede"))
+altura=float(input("digite a altura da parede"))
+s=largura*altura
+t=s/2 
+print("analisando a largura{},e a altura{},a area é igual{},e vai ser gastar {}de tinta".format(largura,altura,s,t))
